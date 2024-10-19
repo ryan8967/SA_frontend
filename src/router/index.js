b8de913564ed45f04da8616ede3446483fb944ea
unknown <tako1201.44.5@gmail.com>
2024-10-20 03:41:00 +0800

    /New2TaskPage

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,8 @@ import TaskPage from "@/pages/TaskPage.vue";
 import ProfilePage from "@/pages/ProfilePage.vue";
 import New1TaskPage from "@/pages/New1TaskPage.vue";
 import Testing1Page from "@/pages/Testing1Page.vue";
+import New2TaskPage from "@/pages/New2TaskPage.vue";
+
 
 const routes = [
   {
@@ -54,6 +56,12 @@ const routes = [
     path: "/test",
     name: "test",
     component: Testing1Page,
+    meta: { requiresAuth: true }, // 需要登入的頁面
+  },
+  {
+    path: "/test2",
+    name: "test2",
+    component: New2TaskPage,
     meta: { requiresAuth: true }, // 需要登入的頁面
   },
 ];
