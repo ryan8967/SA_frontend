c54a0026f88e5f3eb37981952b20ca2349f2bdd9
HAYU9013 <114861748+HAYU9013@users.noreply.github.com>
2024-10-20 01:21:11 +0800

    Revert "temp"
    This reverts commit bf2ed59ee4784f455702c68c343486f260e05618.

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -18,11 +18,8 @@
             <font-awesome-icon :icon="['fas', 'store']" class="icon" />
         </button>
 
-        <!-- <button class="nav-item" @click="navigateTo('Profile')">
+        <button class="nav-item" @click="navigateTo('Profile')">
             <font-awesome-icon :icon="['fas', 'user']" class="icon" />
-        </button> -->
-        <button class="nav-item" @click="navigateTo('CreateActivity')">
-            <font-awesome-icon :icon="['fas', 'users']" class="icon" />
         </button>
     </nav>
 </template>
